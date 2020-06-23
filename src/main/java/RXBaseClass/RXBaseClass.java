495f35ba283d9d3a94e2e54e005728d536b7e240
Sutradhar <jayanta.sutradhar@rakuten.com>
2020-06-23 15:26:20 +0530

    code cleanup

--- src/main/java/RXBaseClass/RXBaseClass.java
+++ src/main/java/RXBaseClass/RXBaseClass.java
@@ -8,11 +8,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.events.WebDriverEventListener;
-
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import RXUtitities.Configuration;
 import RXUtitities.EventListener;
@@ -78,6 +80,37 @@ public class RXBaseClass {
 		driver.manage().timeouts().implicitlyWait(RXUtile.IMPLI_WAIT,TimeUnit.SECONDS);
 		driver.get(prop.getProperty("url"));
 		
+	}
+	
+	
+	public boolean waitForJSandJQueryToLoad() {
+
+	    WebDriverWait wait = new WebDriverWait(driver, 30);
+
+	    // wait for jQuery to load
+	    ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
+	      @Override
+	      public Boolean apply(WebDriver driver2) {
+	        try {
+	          return ((Long)((JavascriptExecutor) driver).executeScript("return jQuery.active") == 0);
+	        }
+	        catch (Exception e) {
+	          // no jQuery present
+	          return true;
+	        }
+	      }
+	    };
+
+	    // wait for Javascript to load
+	    ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+	      @Override
+	      public Boolean apply(WebDriver driver2) {
+	        return ((JavascriptExecutor) driver).executeScript("return document.readyState")
+	        .toString().equals("complete");
+	      }
+	    };
+
+	  return wait.until(jQueryLoad) && wait.until(jsLoad);
 	}
 
 }
