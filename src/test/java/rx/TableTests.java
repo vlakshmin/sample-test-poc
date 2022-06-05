a5f440b40c8fb7781b277dc9572f5e90fea41892
Yuliia Podust <os-yuliia.podust@rakuten.com>
2022-06-05 23:24:20 +0300

    table components

--- src/test/java/rx/TableTests.java
+++ src/test/java/rx/TableTests.java
@@ -104,8 +104,8 @@ public class TableTests extends BaseTest {
                 .selectRadioButton(tableOptions.getStatusItemRadio(Statuses.ACTIVE))
                 .clickOnWebElement(tableOptions.getTableOptionsBtn())
                 .selectFromDropdown(tablePagination.getPageMenu(), tablePagination.getRowNumbersList(), "10")
-                .waitAndValidate(visible, publishersPage.getTableProgressBar())
-          //      .waitAndValidate(not(visible), publishersPage.getTableProgressBar())
+          //      .waitAndValidate(visible, publishersPage.getTableProgressBar())
+                .waitAndValidate(not(visible), publishersPage.getTableProgressBar())
                 .validateList(table.getRows(), 10)
                 .validateListContainsTextOnly(table.getCustomCells(ColumnNames.ACTIVE),
                         Statuses.ACTIVE.getStatus())
@@ -129,10 +129,11 @@ public class TableTests extends BaseTest {
                 .and()
                 .setValueWithClean(table.getSearch(), publisher.getName())
                 .clickEnterButton(table.getSearch())
-           //     .waitAndValidate(visible, publishersPage.getTableProgressBar())
+                .waitAndValidate(visible, publishersPage.getTableProgressBar())
                 .waitAndValidate(not(visible), publishersPage.getTableProgressBar())
                 .validateListContainsTextOnly(table.getCustomCells(ColumnNames.PUBLISHER),
                        publisher.getName())
+                .and()
                 //.clickOnWebElement(publishersPage.getPublisherItemByName(publisher.getName()).getPublisherName())
                 .clickOnTableCell(ColumnNames.PUBLISHER, publisher.getName())
                 .waitSideBarOpened()
