bcdef4774bbf033e40e82eb8c32b9f126a6e9c0e
YuliiaPodust <os-yuliia.podust@rakuten.com>
2022-08-19 18:52:18 +0300

    added ProtectionsPage, ProtectionsPageElements, ProtectionsCheckPagination; formatted xml for run

---
+++ src/test/java/rx/protections/ProtectionsCheckPagination.java
@@ -0,0 +1,116 @@
+package rx.protections;
+
+import api.dto.rx.protection.Protection;
+import com.codeborne.selenide.testng.ScreenShooter;
+import io.qameta.allure.Step;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.*;
+import pages.Path;
+import pages.protections.ProtectionsPage;
+import rx.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static api.preconditionbuilders.ProtectionsPrecondition.protection;
+import static api.preconditionbuilders.PublisherPrecondition.publisher;
+import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.visible;
+import static configurations.User.TEST_USER;
+import static configurations.User.USER_FOR_DELETION;
+import static managers.TestManager.testStart;
+
+@Slf4j
+@Listeners({ScreenShooter.class})
+public class ProtectionsCheckPagination extends BaseTest {
+    private ProtectionsPage protectionsPage;
+
+    private int totalProtections;
+    private List<Protection> listProtections;
+
+    public ProtectionsCheckPagination() {
+        protectionsPage = new ProtectionsPage();
+    }
+
+    @BeforeClass
+    private void init() {
+        if (getTotalProtections() < 100) generateProtection();
+
+        totalProtections = getTotalProtections();
+    }
+
+    @BeforeMethod
+    private void login() {
+        testStart().given().openDirectPath(Path.PROTECTIONS).logIn(TEST_USER).waitAndValidate(disappear, protectionsPage.getNuxtProgress()).testEnd();
+    }
+
+    @Test(description = "Verify Pagination: 100 rows per page")
+    public void checkPagination100() {
+        verifyPagination(100);
+    }
+
+    @Test(description = "Verify Pagination: 50 rows per page")
+    public void checkPagination50() {
+        verifyPagination(50);
+    }
+
+    @Test(description = "Verify Pagination: 25 rows per page")
+    public void checkPagination25() {
+        verifyPagination(25);
+    }
+
+    @Test(description = "Verify Pagination: 20 rows per page")
+    public void checkPagination20() {
+        verifyPagination(20);
+    }
+
+    @Test(description = "Verify Pagination: 15 rows per page")
+    public void checkPagination15() {
+        verifyPagination(15);
+    }
+
+    @Test(description = "Verify Pagination: 10 rows per page")
+    public void checkPagination10() {
+        verifyPagination(10);
+    }
+
+    @Step("Verify pagination {0}")
+    private void verifyPagination(Integer rowsPerPage) {
+        var tablePagination = protectionsPage.getProtectionsTable().getTablePagination();
+        var tableData = protectionsPage.getProtectionsTable().getTableData();
+
+        testStart().and(String.format("Select %s rows per page", rowsPerPage)).scrollIntoView(tablePagination.getPageMenu()).selectFromDropdown(tablePagination.getPageMenu(), tablePagination.getRowNumbersList(), rowsPerPage.toString()).waitLoading(visible, protectionsPage.getTableProgressBar()).waitLoading(disappear, protectionsPage.getTableProgressBar()).then(String.format(String.format("Validate that text in table footer '1-%s of %s'", rowsPerPage, totalProtections))).validateContainsText(tablePagination.getPaginationPanel(), String.format(String.format("1-%s of %s", rowsPerPage, totalProtections))).then(String.format("Rows in table page equals %s", rowsPerPage)).validateListSize(tableData.getRows(), rowsPerPage).and("Click on Next page").scrollIntoView(tablePagination.getNext()).clickOnWebElement(tablePagination.getNext()).then(String.format(String.format("Validate that text in table footer '%s-%s of %s'", rowsPerPage + 1, Math.min(rowsPerPage * 2, totalProtections), totalProtections))).validateContainsText(tablePagination.getPaginationPanel(), String.format(String.format("%s-%s of %s", rowsPerPage + 1, Math.min(rowsPerPage * 2, totalProtections), totalProtections))).then(String.format("Rows in table page equals %s", rowsPerPage)).validateListSize(tableData.getRows(), rowsPerPage).and("Click on Previous page").scrollIntoView(tablePagination.getPrevious()).clickOnWebElement(tablePagination.getPrevious()).then(String.format(String.format("Validate that text in table footer '1-%s of %s'", rowsPerPage, totalProtections))).validateContainsText(tablePagination.getPaginationPanel(), String.format(String.format("1-%s of %s", rowsPerPage, totalProtections))).then(String.format("Rows in table page equals %s", rowsPerPage)).validateListSize(tableData.getRows(), rowsPerPage).and("Logout").logOut().testEnd();
+    }
+
+    @AfterClass
+    private void deleteEntities() {
+        if (listProtections != null) {
+            for (Protection protection : listProtections) {
+                deleteProtection(protection.getId());
+                deletePublisher(protection.getPublisherId());
+            }
+        }
+    }
+
+    private void deleteProtection(Integer id) {
+        protection().setCredentials(USER_FOR_DELETION).deleteProtection(id);
+    }
+
+    private void deletePublisher(Integer id) {
+        publisher().setCredentials(USER_FOR_DELETION).deletePublisher(id);
+    }
+
+    private int getTotalProtections() {
+
+        return protection().getAllProtectionsList().build().getProtectionsGetAllResponse().getTotal();
+    }
+
+    private void generateProtection() {
+        listProtections = new ArrayList<>();
+        while (getTotalProtections() < 120) {
+            Protection protection = protection().createNewRandomProtection().build().getProtectionsResponse();
+
+            listProtections.add(protection);
+        }
+    }
+}
