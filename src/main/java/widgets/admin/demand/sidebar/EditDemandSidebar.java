1051441a46657f96962d4c4e9ccb8b2a3327e55c
SergeyShidlovsky <shedlovskys@gmail.com>
2022-10-04 10:02:02 +0300

    Rearranged selectors

--- src/main/java/widgets/admin/demand/sidebar/EditDemandSidebar.java
+++ src/main/java/widgets/admin/demand/sidebar/EditDemandSidebar.java
@@ -14,8 +14,8 @@ public abstract class EditDemandSidebar {
 
     private SelenideElement syncUrlInput = $x(SYNK_URL.getSelector()).as(SYNK_URL.getAlias());
     private SelenideElement bidderInput = $x(BIDDER.getSelector()).as(BIDDER.getAlias());
-    private SelenideElement endpointURIInput = $x(EDDPOINT_URI_INPUT.getSelector()).as(EDDPOINT_URI_INPUT.getAlias());
     private SelenideElement currency = $x(CURRENCY.getSelector()).as(CURRENCY.getAlias());
     private SelenideElement saveButton = $x(SAVE_BUTTON.getSelector()).as(SAVE_BUTTON.getAlias());
+    private SelenideElement endpointURIInput = $x(EDDPOINT_URI_INPUT.getSelector()).as(EDDPOINT_URI_INPUT.getAlias());
     private SelenideElement dataCenterDropdown = $x(DATA_CENTER_DROPDOWN.getSelector()).as(DATA_CENTER_DROPDOWN.getAlias());
 }
