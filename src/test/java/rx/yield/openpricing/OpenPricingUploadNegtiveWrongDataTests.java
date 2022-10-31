5821ded70f8ff997508c0f835bffda8ccca222f7
YuliiaPodust <os-yuliia.podust@rakuten.com>
2022-10-31 16:44:24 +0200

    regression v1.26.0

--- src/test/java/rx/yield/openpricing/OpenPricingUploadNegtiveWrongDataTests.java
+++ src/test/java/rx/yield/openpricing/OpenPricingUploadNegtiveWrongDataTests.java
@@ -31,7 +31,7 @@ import static zutils.FakerUtils.captionWithSuffix;
 
 @Slf4j
 @Listeners({ScreenShooter.class})
-public class OpenPricingUploadNegtiveTests extends BaseTest {
+public class OpenPricingUploadNegtiveWrongDataTests extends BaseTest {
 
     private Publisher publisher;
     private OpenPricingPage openPricingPage;
@@ -43,7 +43,7 @@ public class OpenPricingUploadNegtiveTests extends BaseTest {
 
     private final String RESOURCES_DIRECTORY = "src/test/resources/csvfiles/openpricing/";
 
-    public OpenPricingUploadNegtiveTests() {
+    public OpenPricingUploadNegtiveWrongDataTests() {
         openPricingPage = new OpenPricingPage();
         openPricingUploadSidebar = new UpdateExistingOpenPricingRulesSidebar();
         openPricingSidebar = new EditOpenPricingSidebar();
@@ -73,90 +73,44 @@ public class OpenPricingUploadNegtiveTests extends BaseTest {
                 .testEnd();
     }
 
-    @Epic("v1.26.0/GS-3083")
-    @Test(description = "Negative: The uploaded file is not a CSV file")
-    private void uploadIsNotCSVNegative() {
-
-        uploadData("non-csv.shell");
-
-        testStart()
-                .waitAndValidate(visible, openPricingPage.getToasterMessage().getPanelError())
-                .clickOnWebElement(openPricingPage.getToasterMessage().getViewErrorDetails())
-                .validate(openPricingPage.getToasterMessage().getMessageError(), "{ \"code\": 415, \"message\": \"unsupported media type\", " +
-                        "\"error\": \"The uploaded file is not a CSV file\" }")
-                .clickOnWebElement(openPricingPage.getToasterMessage().getRemoveIcon())
-                .waitAndValidate(not(visible), openPricingPage.getToasterMessage().getPanelError())
-                .testEnd();
-        closeSideBar();
+    @DataProvider(name = "Negative Upload")
+    public Object[][] uploadFilesNegative() {
+        return new Object[][]{
+                {"by too large decimal.csv", "Upload CSV update zero floorPrice by too large decimal", "Row 3, Floor Price '9999999.99': A valid price is between 0.00 and 999,999.99"},
+                {"by too large int.csv", "Upload CSV update zero floorPrice by too large int", "Row 3, Floor Price '99999999.00': A valid price is between 0.00 and 999,999.99"},
+                {"by negative integer.csv", "Upload CSV update zero floorPrice by negative int", "Row 3, Floor Price '-999999.00': A valid price is between 0.00 and 999,999.99"},
+                {"by negative decimal.csv", "Upload CSV update zero floorPrice by negative decimal", "Row 3, Floor Price '-9999.99': A valid price is between 0.00 and 999,999.99"},
+                {"by negative zero.csv", "Upload CSV update zero floorPrice by negative zero", "Row 3, Floor Price '-0.01': A valid price is between 0.00 and 999,999.99"},
+                {"by string value.csv", "Upload CSV update zero floorPrice by String value", "the CSV file is incorrectly formatted"},
+                {"by empty floor price.csv", "Upload CSV update floorPrice by empty value", "Row 3, Floor Price '': not a valid value"},
+                {"by empty open pricing name.csv", "Upload CSV update by empty open-pricing-name value", "Row 3, Rule Name '': could not match the open pricing Rule Name to the publisher"},
+                {"by null floor price.csv", "Upload CSV update floorPrice by null value", "Row 3, Floor Price 'null': not a valid value"},
+                {"by empty floor price header.csv", "Upload CSV incorrect floor price header", "The CSV file has incorrect headers"},
+                {"by incorrect quantity of headers.csv", "Upload CSV incorrect quantity of headers", "The CSV file is incorrectly formatted"},
+                {"by incorrect quantity of colunms.csv", "Upload CSV incorrect quantity of columns", "The CSV file does not have the correct number of columns"},
+                {"by empty CSV file.csv", "Upload empty CSV file", "The CSV file is empty"},
+                {"by no data.csv", "Upload CSV file with no data", "The CSV file contains no data"},
+                {"by non-existent among others.csv", "Upload CSV update by non-existent pricing among others",
+                        "Row 4, Rule Name 'upload auto four': could not match the open pricing Rule Name to the publisher"},
+                {"by empty pricing name header.csv", "Upload CSV with empty pricing name header", "the CSV file has incorrect headers"},
+                {"duplicated rule names.csv", "Upload CSV update by duplicated pricing names", "Row 3, Floor Price 'upload auto one': the same Rule Name already exists in row 2"},
+        };
     }
 
     @Epic("v1.26.0/GS-3083")
-    @Test(description = "Negative: check errors if required fields are not selected")
-    private void checkRequiredFields() {
-        var errorsList = openPricingUploadSidebar.getErrorAlert().getErrorsList();
+    @Test(description = "Negative: Update existing open pricing rules", dataProvider = "Negative Upload")
+    private void updateExistingOpenPricingRulesNegative(String filename, String descr, String errorMsg) {
 
-        testStart()
-                .and("Open Upload sidebar")
-                .clickOnWebElement(openPricingPage.getUploadOpenPricingButton())
-                .clickOnWebElement(openPricingPage.getUpdateOpenPricingLink())
-                .waitSideBarOpened()
-                .and("Click 'Update Pricing Rules'")
-                .clickOnWebElement(openPricingUploadSidebar.getUpdateExistingRulesButton())
-                .then("Validate errors for all required fields in Error Panel")
-                .waitAndValidate(visible, openPricingUploadSidebar.getErrorAlert().getErrorPanel())
-                .validateListSize(errorsList, 2)
-                .validateList(errorsList, List.of(
-                        ErrorMessages.PUBLISHER_NAME_ERROR_ALERT.getText(),
-                        ErrorMessages.CSV_FILE_ERROR_ALERT.getText())
-                )
-                .then("Validate error under the 'Publisher' field")
-                .validate(openPricingUploadSidebar.getErrorAlertByFieldName("Publisher Name"), ErrorMessages.PUBLISHER_NAME_ERROR_ALERT.getText())
-                .validate(openPricingUploadSidebar.getErrorAlertByFieldName("CSV"), ErrorMessages.CSV_FILE_ERROR_ALERT.getText())
-                .and(String.format("Select Publisher '%s'", publisher.getName()))
-                .selectFromDropdown(openPricingUploadSidebar.getPublisherInput(),
-                        openPricingUploadSidebar.getPublisherNameDropdownItems(), publisher.getName())
-                .then("Validate error under the 'Publisher field' disappeared")
-                .waitAndValidate(not(visible), openPricingUploadSidebar.getErrorAlertByFieldName("Publisher Name"))
-                .validateListSize(errorsList, 1)
-                .validateList(errorsList, List.of(
-                        ErrorMessages.CSV_FILE_ERROR_ALERT.getText())
-                )
-                .uploadFileFromDialog(openPricingUploadSidebar.getCsvFile(), RESOURCES_DIRECTORY + "/by too large int.csv")
-                .then("Validate errors disappeared")
-                .waitAndValidate(not(visible), openPricingUploadSidebar.getErrorAlertByFieldName("CSV"))
-                .waitAndValidate(not(visible), openPricingUploadSidebar.getErrorAlertByFieldName("Publisher Name"))
-                .validate(not(visible), openPricingUploadSidebar.getErrorAlert().getErrorPanel())
-                .testEnd();
+        log.info(descr);
+
+        uploadData(filename);
+        checkErrorAlert(errorMsg);
         closeSideBar();
-    }
 
-    @Epic("v1.26.0/GS-3083")
-    @Test(description = "Negative: check errors if reselect file")
-    private void checkErrorsDisappearAfterSelectedFile() {
-        var errorsList = openPricingUploadSidebar.getErrorAlert().getErrorsList();
-
-        testStart()
-                .and("Open Upload sidebar")
-                .clickOnWebElement(openPricingPage.getUploadOpenPricingButton())
-                .clickOnWebElement(openPricingPage.getUpdateOpenPricingLink())
-                .waitSideBarOpened()
-                .and(String.format("Select Publisher '%s'", publisher.getName()))
-                .selectFromDropdown(openPricingUploadSidebar.getPublisherInput(),
-                        openPricingUploadSidebar.getPublisherNameDropdownItems(), publisher.getName())
-                .uploadFileFromDialog(openPricingUploadSidebar.getCsvFile(), RESOURCES_DIRECTORY + "/by no data.csv")
-                .and("Click 'Update Pricing Rules'")
-                .clickOnWebElement(openPricingUploadSidebar.getUpdateExistingRulesButton())
-                .then("Validate error under the 'CSV' field appeared")
-                .validateListSize(errorsList, 1)
-                .validateList(errorsList, List.of(
-                        "the CSV file contains no data")
-                )
-                .and("Select CSV again")
-                .uploadFileFromDialog(openPricingUploadSidebar.getCsvFile(), RESOURCES_DIRECTORY + "/by too large int.csv")
-                .then("Validate errors disappeared")
-                .validate(not(visible), openPricingUploadSidebar.getErrorAlert().getErrorPanel())
-                .testEnd();
-        closeSideBar();
+        openPricingList
+                .stream()
+                .forEach(e ->
+                        checkDataIsNotChanged(e.getName()));
     }
 
     @Step("Check Error Message")
@@ -208,7 +162,7 @@ public class OpenPricingUploadNegtiveTests extends BaseTest {
         closeSideBar();
     }
 
-    @Step("Close Sidebar")
+    @Step("Close sidebar")
     private void closeSideBar() {
 
         testStart()
