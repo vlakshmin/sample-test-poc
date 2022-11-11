d83b5abdb0805a151c27e2949e4e40084ad6705e
vlakshmin <5708803+vlakshmin@users.noreply.github.com>
2022-11-10 20:21:34 -0800

    Add files via upload

---
+++ main/java/page/HomePage.java
@@ -0,0 +1,37 @@
+package page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+
+    private final By usernameLabel = By.xpath("//div[@class='header_user_info']//span");
+    private final By searchTextBox = By.id("search_query_top");
+    private final By searchButton = By.cssSelector("button[name='submit_search']");
+    private final By trialsAndDemo = By.className("FlagCta_title__gUjXo");
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getLoggedInUsername() {
+        return getElement(usernameLabel).getText();
+    }
+
+    public void enterSearchItem(String itemName) {
+        sendKeys(searchTextBox, itemName);
+    }
+
+    public void enterSearchButton() {
+        click(searchButton);
+    }
+
+    public void searchItem(String itemName) {
+        enterSearchItem(itemName);
+        enterSearchButton();
+    }
+    
+    public void clickTrials() {
+    	getElement(trialsAndDemo).click();
+    }
+}
