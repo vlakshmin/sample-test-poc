b8db8ade4323ba356cf3935dc8ef07881f9b3601
venkatalak.nandipati <venkatalak.nandipati@rakuten.com>
2022-11-14 12:17:14 -0800

    user pagination and user page sorting tests

---
+++ src/test/java/rx/admin/users/UsersCheckPaginationTests.java
@@ -0,0 +1,173 @@
+package rx.admin.users;
+
+import api.dto.rx.admin.user.UserDto;
+import api.dto.rx.admin.user.UserRole;
+import com.codeborne.selenide.testng.ScreenShooter;
+import io.qameta.allure.Step;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.*;
+import pages.Path;
+import pages.admin.users.UsersPage;
+import rx.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static api.preconditionbuilders.UsersPrecondition.user;
+import static api.preconditionbuilders.PublisherPrecondition.publisher;
+import static com.codeborne.selenide.Condition.*;
+import static configurations.User.TEST_USER;
+import static configurations.User.USER_FOR_DELETION;
+import static managers.TestManager.testStart;
+import static zutils.FakerUtils.captionWithSuffix;
+
+@Slf4j
+@Listeners({ScreenShooter.class})
+public class UsersCheckPaginationTests extends BaseTest {
+    private UsersPage usersPage;
+
+    private int totalUsers;
+    private List<UserDto> listUsers;
+
+    public UsersCheckPaginationTests() {
+    	usersPage = new UsersPage();
+    }
+
+    @BeforeClass
+    private void init() {
+        if (getTotalUsers() < 200) generateUsers();
+
+        totalUsers = getTotalUsers();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    private void login() {
+        testStart()
+                .given()
+                .openDirectPath(Path.USER)
+                .logIn(TEST_USER)
+                .waitAndValidate(disappear, usersPage.getNuxtProgress())
+                .testEnd();
+    }
+
+    @Test(description = "Verify Pagination: 100 rows per page", alwaysRun = true, priority = 1)
+    public void checkPagination100() {
+        verifyPagination(100);
+    }
+
+    @Test(description = "Verify Pagination: 50 rows per page", alwaysRun = true, priority = 4)
+    public void checkPagination50() {
+        verifyPagination(50);
+    }
+
+    @Test(description = "Verify Pagination: 25 rows per page", alwaysRun = true, priority = 5)
+    public void checkPagination25() {
+        verifyPagination(25);
+    }
+
+    @Test(description = "Verify Pagination: 20 rows per page", alwaysRun = true, priority = 6)
+    public void checkPagination20() {
+        verifyPagination(20);
+    }
+
+    @Test(description = "Verify Pagination: 15 rows per page", alwaysRun = true, priority = 3)
+    public void checkPagination15() {
+        verifyPagination(15);
+    }
+
+    @Test(description = "Verify Pagination: 10 rows per page", alwaysRun = true, priority = 2)
+    public void checkPagination10() {
+        verifyPagination(10);
+    }
+
+    @Step("Verify pagination {0}")
+    private void verifyPagination(Integer rowsPerPage) {
+        var tablePagination = usersPage.getUsersTable().getTablePagination();
+        var tableData = usersPage.getUsersTable().getTableData();
+
+        testStart()
+                .and(String.format("Select %s rows per page", rowsPerPage))
+                .scrollIntoView(tablePagination.getPageMenu())
+                .selectFromDropdown(tablePagination.getPageMenu(),
+                        tablePagination.getRowNumbersList(), rowsPerPage.toString())
+                .waitLoading(disappear, usersPage.getTableProgressBar())
+                .then(String.format(String.format("Validate that text in table footer '1-%s of %s'", rowsPerPage,
+                        totalUsers)))
+                .validateContainsText(tablePagination.getPaginationPanel(),
+                        String.format(String.format("1-%s of %s", rowsPerPage, totalUsers)))
+                .then(String.format("Rows in table page equals %s", rowsPerPage))
+                .validateListSize(tableData.getRows(), rowsPerPage)
+                .and("Click on Next page")
+                .scrollIntoView(tablePagination.getNext())
+                .clickOnWebElement(tablePagination.getNext())
+                .then(String.format(String.format("Validate that text in table footer '%s-%s of %s'",
+                        rowsPerPage + 1, Math.min(rowsPerPage * 2, totalUsers), totalUsers)))
+                .validateContainsText(tablePagination.getPaginationPanel(),
+                        String.format(String.format("%s-%s of %s",
+                                rowsPerPage + 1, Math.min(rowsPerPage * 2, totalUsers), totalUsers)))
+                .then(String.format("Rows in table page equals %s", rowsPerPage))
+                .validateListSize(tableData.getRows(),rowsPerPage)
+                .and("Click on Previous page")
+                .scrollIntoView(tablePagination.getPrevious())
+                .clickOnWebElement(tablePagination.getPrevious())
+                .then(String.format(String.format("Validate that text in table footer '1-%s of %s'",
+                        rowsPerPage, totalUsers)))
+                .validateContainsText(tablePagination.getPaginationPanel(),
+                        String.format(String.format("1-%s of %s",
+                                rowsPerPage, totalUsers)))
+                .then(String.format("Rows in table page equals %s", rowsPerPage))
+                .validateListSize(tableData.getRows(), rowsPerPage)
+                .testEnd();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logout(){
+        testStart()
+                .and("Logout")
+                .logOut()
+                .testEnd();
+    }
+
+    @AfterClass(alwaysRun = true)
+    private void deleteEntities() {
+
+        if (listUsers != null) {
+            for (UserDto users : listUsers) {
+                deleteUser(users.getId());
+            }
+        }
+    }
+
+    private void deleteUser(Integer id) {
+        user()
+                .setCredentials(USER_FOR_DELETION)
+                .deleteUser(id);
+    }
+
+    private void deletePublisher(Integer id) {
+        publisher()
+                .setCredentials(USER_FOR_DELETION)
+                .deletePublisher(id);
+    }
+
+    private int getTotalUsers() {
+        return user()
+                .getAllUsers()
+                .build()
+                .getUserGetAllResponse()
+                .getTotal();
+    }
+
+    private void generateUsers() {
+
+        listUsers = new ArrayList<>();
+        while (getTotalUsers() < 210) {
+            UserDto user = user()
+            		.createNewUser(UserRole.SINGLE_PUBLISHER)
+                    .build()
+                    .getUserResponse();
+
+            listUsers.add(user);
+        }
+    }
+}
---
+++ src/test/java/rx/admin/users/UsersSortingTableTests.java
@@ -0,0 +1,285 @@
+package rx.admin.users;
+
+import api.dto.rx.admin.user.UserDto;
+import api.dto.rx.admin.user.UserRole;
+import api.preconditionbuilders.UsersPrecondition;
+import com.codeborne.selenide.testng.ScreenShooter;
+import io.qameta.allure.Step;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.*;
+import pages.Path;
+import pages.admin.users.UsersPage;
+import rx.BaseTest;
+import widgets.common.table.ColumnNames;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.visible;
+import static configurations.User.TEST_USER;
+import static managers.TestManager.testStart;
+
+@Slf4j
+@Listeners({ScreenShooter.class})
+public class UsersSortingTableTests extends BaseTest {
+
+    private int totalUsers;
+    private List<String> sortIdsByAsc;
+    private List<String> sortIdsByDesc;
+    private List<String> sortNamesByAsc;
+    private List<String> sortNamesByDesc;
+
+    private List<String> sortEmailByDesc;
+    private List<String> sortEmailByAsc;
+    private UsersPage usersPage;
+
+    private static final String ASC = "ascending";
+    private static final String DESC = "descending";
+
+    public UsersSortingTableTests() {
+        usersPage = new UsersPage();
+    }
+
+    @BeforeClass
+    private void loginAndCreateExpectedResuts() {
+
+        if (getTotalUsers() < 60) generateUsers();
+
+        totalUsers = getTotalUsers();
+
+        //expected results for Media Name column
+        sortNamesByDesc = getNamesByDesc();
+        sortNamesByAsc = getNamesByAsc();
+
+        //Expected result for ID column
+        sortIdsByAsc = getIdsByAsc();
+        sortIdsByDesc = getIdsByDesc();
+
+        //Expected result for  Publisher Name column
+        sortEmailByAsc = getUserEmailByAsc();
+        sortEmailByDesc = getUserEmailByDesc();
+    }
+
+    @Test(testName = "Sorting 'ID' column by descending", alwaysRun = true)
+    public void userSortingByIdDesc() {
+        sortByDescColumnByName(ColumnNames.ID);
+        validateSortData(ColumnNames.ID, DESC, sortIdsByDesc);
+    }
+
+    @Test(testName = "Sorting 'ID' column by ascending", alwaysRun = true)
+    public void userSortingByIdAsc() {
+
+        sortByAscColumnByName(ColumnNames.ID);
+        validateSortData(ColumnNames.ID, ASC, sortIdsByAsc);
+    }
+
+    @Test(testName = "Sorting 'User Name' column by descending", alwaysRun = true)
+    public void userSortingByUserNameDesc() {
+        sortByDescColumnByName(ColumnNames.NAME);
+        validateSortData(ColumnNames.NAME, DESC, sortNamesByDesc);
+    }
+
+    @Test(testName = "Sorting 'User Name' column by ascending", alwaysRun = true)
+    public void userSortingByUserNameAsc() {
+        sortByAscColumnByName(ColumnNames.NAME);
+        validateSortData(ColumnNames.NAME, ASC, sortNamesByAsc);
+    }
+
+    @Test(testName = "Sorting 'Email' column by ascending", alwaysRun = true)
+    public void userSortingByEmailAsc() {
+        sortByAscColumnByName(ColumnNames.EMAIL);
+        validateSortData(ColumnNames.EMAIL, ASC, sortEmailByAsc);
+    }
+
+    @Test(testName = "Sorting 'Email' column by descending", alwaysRun = true)
+    public void userSortingByEmailDesc() {
+        sortByDescColumnByName(ColumnNames.EMAIL);
+        validateSortData(ColumnNames.EMAIL, DESC, sortEmailByDesc);
+    }
+
+    @BeforeMethod
+    private void login() {
+        var table = usersPage.getUsersTable().getTableOptions();
+        var tableData = usersPage.getUsersTable().getTableData();
+        testStart()
+                .given()
+                .openDirectPath(Path.USER)
+                .logIn(TEST_USER)
+                .waitAndValidate(disappear, usersPage.getNuxtProgress())
+                .testEnd();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    private void logOut() {
+        testStart()
+                .given()
+                .logOut()
+                .testEnd();
+    }
+
+    @Step("Sort column {0} by DESC")
+    private void sortByDescColumnByName(ColumnNames columnName) {
+        var tableData = usersPage.getUsersTable().getTableData();
+
+        testStart()
+                .given()
+                .and(String.format("Sort column '%s'", columnName))
+                .clickOnWebElement(tableData.getColumnHeader(columnName.getName()))
+                .testEnd();
+
+        if (columnName.getName().equals("ID")) {
+            testStart()
+                    .clickOnWebElement(tableData.getColumnHeader(columnName.getName()))
+                    .testEnd();
+        }
+
+        testStart()
+                .then("Ensure that sort by descending: validate column attribute value")
+                .validateAttribute(tableData.getColumnHeader(columnName.getName()),
+                        "aria-sort", ASC)
+                .clickOnWebElement(tableData.getColumnHeader(columnName.getName()))
+                .validateAttribute(tableData.getColumnHeader(columnName.getName()),
+                        "aria-sort", DESC)
+                .waitAndValidate(disappear, usersPage.getNuxtProgress())
+                .testEnd();
+
+    }
+
+    @Step("Sort column {0} by ASC")
+    private void sortByAscColumnByName(ColumnNames columnName) {
+        var tableData = usersPage.getUsersTable().getTableData();
+        testStart()
+                .given()
+                .and(String.format("Sort column '%s'", columnName))
+                .clickOnWebElement(tableData.getColumnHeader(columnName.getName()))
+                .testEnd();
+
+        if (columnName.getName().equals("ID")) {
+            testStart()
+                    .clickOnWebElement(tableData.getColumnHeader(columnName.getName()))
+                    .testEnd();
+        }
+
+        testStart()
+                .then("Ensure that sort by ascending: validate column attribute value")
+                .validateAttribute(tableData.getColumnHeader(columnName.getName()),
+                        "aria-sort", ASC)
+                .waitAndValidate(disappear, usersPage.getNuxtProgress())
+                .testEnd();
+
+    }
+
+    @Step("Validate data in column {0} sorted by {1}")
+    private void validateSortData(ColumnNames columnName, String sortType, List<String> expectedResultList) {
+        var tableData = usersPage.getUsersTable().getTableData();
+        var tablePagination = usersPage.getUsersTable().getTablePagination();
+
+        testStart()
+                .given()
+                .waitAndValidate(disappear, usersPage.getNuxtProgress())
+                .and("Select 50 row per page")
+                .scrollIntoView(tablePagination.getPageMenu())
+                .selectFromDropdown(tablePagination.getPageMenu(),
+                        tablePagination.getRowNumbersList(), "50")
+                .waitLoading(visible, usersPage.getTableProgressBar())
+                .waitLoading(disappear, usersPage.getTableProgressBar())
+                .then(String.format("Validate that text in table footer '1-50 of %s'",
+                        totalUsers))
+                .validateContainsText(tablePagination.getPaginationPanel(),
+                        String.format("1-50 of %s", totalUsers))
+                .then(String.format("Validate data in column '%s' should be sorted by $s",
+                        columnName.getName(), sortType))
+                .validateList(tableData.getCustomCells(columnName),
+                        expectedResultList.subList(0, 50))
+                .and("Check next page")
+                .clickOnWebElement(tablePagination.getNext())
+                .waitLoading(visible, usersPage.getTableProgressBar())
+                .waitLoading(disappear, usersPage.getTableProgressBar())
+                .then(String.format("Validate that text in table footer '51-%s of %s'",
+                        Math.min(100, totalUsers), totalUsers))
+                .validateContainsText(tablePagination.getPaginationPanel(),
+                        String.format("51-%s of %s", Math.min(100, totalUsers), totalUsers))
+                .then(String.format("Validate data in column '%s' should be sorted by %s", columnName.getName(), sortType))
+                .validateList(tableData.getCustomCells(columnName),
+                        expectedResultList.subList(50, Math.min(100, totalUsers)))
+
+                .testEnd();
+    }
+
+
+    private int getTotalUsers() {
+
+        return UsersPrecondition.user()
+                .getAllUsers()
+                .build()
+                .getUserGetAllResponse()
+                .getTotal();
+    }
+
+    private List<String> getNamesByDesc() {
+
+        return getAllItemsByParams("name-desc").stream()
+                .map(UserDto::getName)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getNamesByAsc() {
+
+        return getAllItemsByParams("name-asc").stream()
+                .map(UserDto::getName)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getIdsByAsc() {
+
+        return getAllItemsByParams("id-asc").stream()
+                .map(UserDto::getId)
+                .map(x -> x.toString())
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getIdsByDesc() {
+
+        return getAllItemsByParams("id-desc").stream()
+                .map(UserDto::getId)
+                .map(x -> x.toString())
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getUserEmailByAsc() {
+
+        return getAllItemsByParams("mail-asc").stream()
+                .map(UserDto::getMail)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getUserEmailByDesc() {
+
+        return getAllItemsByParams("mail-desc").stream()
+                .map(UserDto::getMail)
+                .collect(Collectors.toList());
+    }
+    private List<UserDto> getAllItemsByParams(String strParams) {
+        Map<String, Object> queryParams = new HashMap();
+        queryParams.put("sort", strParams);
+
+        return UsersPrecondition.user()
+                .getUsersWithFilter(queryParams)
+                .build()
+                .getUserGetAllResponse()
+                .getItems();
+    }
+
+    private void generateUsers() {
+
+        while (getTotalUsers() < 60) {
+            UsersPrecondition.user()
+                    .createNewUser(UserRole.SINGLE_PUBLISHER)
+                    .build();
+        }
+    }
+}
