40d0aa116e6450086569ffee1cb2cbcb2c2350ac
SergeyShidlovsky <shedlovskys@gmail.com>
2022-11-18 18:47:50 +0300

    Added Base calss for filters

--- src/main/java/widgets/common/filter/activebooleanfilter/ActiveBooleanFilter.java
+++ src/main/java/widgets/common/filter/activebooleanfilter/ActiveBooleanFilter.java
@@ -2,6 +2,7 @@ package widgets.common.filter.activebooleanfilter;
 
 import com.codeborne.selenide.SelenideElement;
 import lombok.Getter;
+import widgets.common.filter.abstractt.BaseFilter;
 
 import static com.codeborne.selenide.Selenide.$x;
 import static widgets.common.filter.activebooleanfilter.ActiveBooleanFilterElements.*;
@@ -11,13 +12,10 @@ import static widgets.common.filter.activebooleanfilter.ActiveBooleanFilterEleme
  * Keep Selectors of UI elements in {@link ActiveBooleanFilterElements}
  */
 @Getter
-public class ActiveBooleanFilter {
+public class ActiveBooleanFilter extends BaseFilter {
 
-    private SelenideElement filterHeaderLabel = $x(FILTER_HEADER.getSelector()).as(FILTER_HEADER.getAlias());
-    private SelenideElement backButton = $x(BACK_BUTTON.getSelector()).as(BACK_BUTTON.getAlias());
+
     private SelenideElement activeRadioButtonButton = $x(ACTIVE_RADIO_BUTTON.getSelector()).as(ACTIVE_RADIO_BUTTON.getAlias());
     private SelenideElement inactiveRadioButton = $x(INACTIVE_RADIO_BUTTON.getSelector()).as(INACTIVE_RADIO_BUTTON.getAlias());
-    private SelenideElement cancelButton = $x(CANCEL_BUTTON.getSelector()).as(CANCEL_BUTTON.getAlias());
-    private SelenideElement submitButton = $x(SUBMIT_BUTTON.getSelector()).as(SUBMIT_BUTTON.getAlias());
 
 }
--- src/main/java/widgets/common/filter/activebooleanfilter/ActiveBooleanFilterElements.java
+++ src/main/java/widgets/common/filter/activebooleanfilter/ActiveBooleanFilterElements.java
@@ -8,11 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ActiveBooleanFilterElements {
 
-    BACK_BUTTON("'Back' button in 'Active/Inactive' Filter","//div[@role='menu']//i[contains(@class,'back')]"),
-    CANCEL_BUTTON("'Cancel' button in in 'Active/Inactive' Filter","//div[@role='menu']//span[text()='Cancel']/.."),
-    SUBMIT_BUTTON("'Cancel' button in in 'Active/Inactive' Filter","//div[@role='menu']//span[text()='Submit']/.."),
     ACTIVE_RADIO_BUTTON("'Active' button in in 'Active/Inactive' Filter","//div[@role='menu']//label[text()='Active']/..//input"),
-    FILTER_HEADER("'Filter Header Label in 'Active/Inactive' Filter", "//div[@role='menu']//div[contains(@class,'filter-title')]"),
     INACTIVE_RADIO_BUTTON("'Active' button in in 'Active/Inactive' Filter","//div[@role='menu']//label[text()='Inactive']/..//input");
 
     private String alias;
