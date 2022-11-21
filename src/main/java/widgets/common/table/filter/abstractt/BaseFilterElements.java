1595e1a6908da897b934ef49374c70d33de62613
SergeyShidlovsky <shedlovskys@gmail.com>
2022-11-21 16:35:53 +0300

    Fixed TableDataTests

--- src/main/java/widgets/common/table/filter/abstractt/BaseFilterElements.java
+++ src/main/java/widgets/common/table/filter/abstractt/BaseFilterElements.java
@@ -10,7 +10,7 @@ public enum BaseFilterElements {
 
     BACK_BUTTON("'Back' button in 'Active/Inactive' Filter","//div[@role='menu']//i[contains(@class,'back')]"),
     CANCEL_BUTTON("'Cancel' button in in 'Active/Inactive' Filter","//div[@role='menu']//span[text()='Cancel']/.."),
-    SUBMIT_BUTTON("'Cancel' button in in 'Active/Inactive' Filter","//div[@role='menu']//span[text()='Submit']/.."),
+    SUBMIT_BUTTON("'Submit' button in in 'Active/Inactive' Filter","//div[@role='menu']//span[text()='Submit']/.."),
     FILTER_HEADER("'Filter Header Label in 'Active/Inactive' Filter", "//div[@role='menu']//div[contains(@class,'filter-title')]");
 
     private String alias;
