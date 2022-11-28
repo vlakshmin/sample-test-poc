98f8d8513e8d6616761dc215e2fa7e617524d55a
YuliiaPodust <os-yuliia.podust@rakuten.com>
2022-11-28 10:23:36 +0200

    chip Item

--- src/test/java/rx/component/table/FilterChipTests.java
+++ src/test/java/rx/component/table/FilterChipTests.java
@@ -85,6 +85,7 @@ public class FilterChipTests extends BaseTest {
 
         testStart()
                 .and(format("Reset filter %s", ColumnNames.PUBLISHER.getName()))
+
                 .clickOnWebElement(table.getChipItemByName(ColumnNames.PUBLISHER.getName()).getCloseIcon())
                 .then(format("Chip '%s' should be disabled", ColumnNames.PUBLISHER.getName()))
                 .validate(table.getFilterChips().size(),0)
