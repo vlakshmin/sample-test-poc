d3a911c2eea684e1628b9c45b00c21d783add12f
SergeyShidlovsky <shedlovskys@gmail.com>
2022-12-05 14:21:38 +0300

    Fixed part of selectors and relaunched pipeline

--- src/main/java/widgets/common/table/filter/activebooleanfilter/ActiveBooleanFilterElements.java
+++ src/main/java/widgets/common/table/filter/activebooleanfilter/ActiveBooleanFilterElements.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ActiveBooleanFilterElements {
 
-    ACTIVE_RADIO_BUTTON("'Active' button in in 'Active/Inactive' Filter","//div[@role='menu']//label[text()='Active']/../div/div"),
-    INACTIVE_RADIO_BUTTON("'Active' button in in 'Active/Inactive' Filter","//div[@role='menu']//label[text()='Inactive']/../div/div");
+    ACTIVE_RADIO_BUTTON("'Active' button in in 'Active/Inactive' Filter","//div[@role='menu']//label[text()='Active']/..//input"),
+    INACTIVE_RADIO_BUTTON("'Active' button in in 'Active/Inactive' Filter","//div[@role='menu']//label[text()='Inactive']/..//input");
 
     private String alias;
     private String selector;
