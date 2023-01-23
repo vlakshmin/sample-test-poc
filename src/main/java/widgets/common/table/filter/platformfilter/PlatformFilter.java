1b0ca5c9297b328a642f58377e054bf9f1fd4477
SergeyShidlovsky <shedlovskys@gmail.com>
2023-01-23 17:43:00 +0300

    Updated Platform filter object

--- src/main/java/widgets/common/table/filter/platformfilter/PlatformFilter.java
+++ src/main/java/widgets/common/table/filter/platformfilter/PlatformFilter.java
@@ -15,8 +15,17 @@ public class PlatformFilter extends BaseFilter {
 
     private SelenideElement iOSCheckBox = $x(PlatformFilterElements.IOS_PLATFORM_CHECkBOX.getSelector())
             .as(PlatformFilterElements.IOS_PLATFORM_CHECkBOX.getAlias());
+    private SelenideElement ctvWebCheckBox = $x(PlatformFilterElements.CTV_PLATFORM_CHECkBOX.getSelector())
+            .as(PlatformFilterElements.CTV_PLATFORM_CHECkBOX.getAlias());
+    private SelenideElement pcWebCheckBox = $x(PlatformFilterElements.PC_WEB_PLATFORM_CHECkBOX.getSelector())
+            .as(PlatformFilterElements.PC_WEB_PLATFORM_CHECkBOX.getAlias());
     private SelenideElement androidCheckBox = $x(PlatformFilterElements.ANDROID_PLATFORM_CHECkBOX.getSelector())
             .as(PlatformFilterElements.ANDROID_PLATFORM_CHECkBOX.getAlias());
-    private SelenideElement mobileWebCheckBox = $x(PlatformFilterElements.MOBILE_WEB_CHECkBOX.getSelector())
-            .as(PlatformFilterElements.MOBILE_WEB_CHECkBOX.getAlias());
+    private SelenideElement mobileWebCheckBox = $x(PlatformFilterElements.MOBILE_WEB_PLATFORM_CHECkBOX.getSelector())
+            .as(PlatformFilterElements.MOBILE_WEB_PLATFORM_CHECkBOX.getAlias());
+
+    private SelenideElement iOSWebViewCheckBox = $x(PlatformFilterElements.IOS_WEB_VIEW_PLATFORM_CHECkBOX.getSelector())
+            .as(PlatformFilterElements.IOS_WEB_VIEW_PLATFORM_CHECkBOX.getAlias());
+    private SelenideElement androidWebViewCheckBox = $x(PlatformFilterElements.ANDROID_WEB_VIEW_PLATFORM_CHECkBOX.getSelector())
+            .as(PlatformFilterElements.ANDROID_WEB_VIEW_PLATFORM_CHECkBOX.getAlias());
 }
